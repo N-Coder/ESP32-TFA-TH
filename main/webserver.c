93fe62a8fd243ed583a474cfbaaefdd4ba5f71ea
Niko Fink <nfink95@gmail.com>
2018-12-22 20:58:19 +0100

    added HEAD + OPTIONS for /sd_file and more efficient body sending

--- main/webserver.c
+++ main/webserver.c
@@ -18,8 +18,8 @@ esp_err_t current_html_get_handler(httpd_req_t *req) {
                       "<th>session ID</th>"
                       "<th>low battery</th>"
                       "<th>channel</th>"
-                      "<th>Temperature (&deg;C)</th>"
-                      "<th>Humidity (%)</th>"
+                      "<th>temperature (&deg;C)</th>"
+                      "<th>humidity (%)</th>"
                       "</tr>\n");
 
     struct tm timeinfo;
@@ -120,7 +120,7 @@ esp_err_t sd_file_get_handler(httpd_req_t *req) {
     }
     ESP_ERROR_CHECK(ret);
 
-    if (stat(filename, &st) == 0) {
+    if (stat(filename, &st) == 0 && S_ISREG(st.st_mode)) {
         ESP_LOGV(TAG, "Delivering file %s containing %ld bytes", filename, st.st_size);
         if (strcmp(filename + 8, ".CSV") == 0) {
             httpd_resp_set_type(req, "text/csv");
@@ -129,8 +129,15 @@ esp_err_t sd_file_get_handler(httpd_req_t *req) {
         }
         sprintf(length_str, "%ld", st.st_size);
         httpd_resp_set_hdr(req, "Content-Length", length_str);
+        httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
     } else {
         ESP_ERROR_CHECK(httpd_resp_send_404(req));
+        return ESP_OK;
+    }
+
+    if (req->method == HTTP_HEAD) {
+        ESP_ERROR_CHECK(httpd_resp_send_chunk(req, NULL, 0));
+        ESP_LOGV(TAG, "Sent only headers after HEAD request.");
         return ESP_OK;
     }
 
@@ -140,16 +147,23 @@ esp_err_t sd_file_get_handler(httpd_req_t *req) {
         return ESP_OK;
     }
 
+    // TODO Content-Range
     size_t sent = 0;
     while (sent < st.st_size) {
-        if (fgets(http_resp, sizeof(http_resp), f) == NULL) {
-            fclose(f);
-            ESP_ERROR_CHECK(httpd_resp_send_404(req));
-            return ESP_OK;
+        http_resp[0] = 0;
+        size_t len = 0;
+        while (len < 0.8 * sizeof(http_resp) && sent + len < st.st_size) {
+            if (fgets(http_resp + len, sizeof(http_resp) - len, f) == NULL) {
+                fclose(f);
+                ESP_ERROR_CHECK(httpd_resp_send_404(req));
+                return ESP_OK;
+            }
+            size_t newlen = strlen(http_resp);
+            ESP_LOGV(TAG,
+                     "Got %u new bytes of data. Buffer already contains %u of %u bytes. Already sent %u of %ld bytes.",
+                     newlen - len, newlen, sizeof(http_resp), sent, st.st_size);
+            len = newlen;
         }
-        size_t len = strlen(http_resp);
-        ESP_LOGV(TAG, "Got %u (buffer fits %u) new bytes of data. Already sent %u of %ld bytes.",
-                 len, sizeof(http_resp), sent, st.st_size);
         ESP_ERROR_CHECK(httpd_resp_send_chunk(req, http_resp, len));
         sent += len;
     }
@@ -163,6 +177,34 @@ httpd_uri_t sd_file = {
         .uri       = "/sd_file",
         .method    = HTTP_GET,
         .handler   = sd_file_get_handler
+};
+httpd_uri_t sd_file_head = {
+        .uri       = "/sd_file",
+        .method    = HTTP_HEAD,
+        .handler   = sd_file_get_handler
+};
+
+esp_err_t cors_options_handler(httpd_req_t *req) {
+    httpd_resp_set_hdr(req, "Allow", req->user_ctx);
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Methods", req->user_ctx);
+    char headers[128] = {0};
+    esp_err_t ret = httpd_req_get_hdr_value_str(req, headers, "Access-Control-Request-Headers", sizeof(headers));
+    ESP_LOGV(TAG, "Access-Control-Request-Headers %X %s", ret, headers);
+    if (ret == ESP_OK) {
+        httpd_resp_set_hdr(req, "Access-Control-Allow-Headers", headers);
+    } else {
+        httpd_resp_set_hdr(req, "Access-Control-Allow-Headers", "if-none-match,range");
+    }
+    httpd_resp_send(req, NULL, 0);
+    return ESP_OK;
+}
+
+httpd_uri_t sd_file_options = {
+        .uri       = "/sd_file",
+        .method    = HTTP_OPTIONS,
+        .handler   = cors_options_handler,
+        .user_ctx = "GET, OPTIONS, HEAD"
 };
 
 httpd_handle_t start_webserver(void) {
@@ -181,5 +223,7 @@ httpd_handle_t start_webserver(void) {
     httpd_register_uri_handler(server, &current_html);
     httpd_register_uri_handler(server, &current_json);
     httpd_register_uri_handler(server, &sd_file);
+    httpd_register_uri_handler(server, &sd_file_head);
+    httpd_register_uri_handler(server, &sd_file_options);
     return server;
 }
