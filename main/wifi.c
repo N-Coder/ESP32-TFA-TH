4c94734583b2ca9b6502e0900301dfcdaada83eb
Niko Fink <nfink95@gmail.com>
2019-01-01 15:30:59 +0100

    improved WiFi handling and function for ensuring connectivity

--- main/wifi.c
+++ main/wifi.c
@@ -9,13 +9,16 @@
 static const char *TAG = "ESP32-TFA-TH/WiFi";
 
 static EventGroupHandle_t wifi_event_group;
-const int CONNECTED_BIT = BIT0;
-const int GOT_IP_BIT = BIT1;
+const int READY_BIT = BIT0;
+const int CONNECTED_BIT = BIT1;
+const int GOT_IP_BIT = BIT2;
 
 esp_err_t event_handler(void *ctx, system_event_t *event) {
     if (event->event_id == SYSTEM_EVENT_STA_START) {
+        ESP_LOGI(TAG, "WiFi driver ready, connecting...");
         xEventGroupClearBits(wifi_event_group, CONNECTED_BIT | GOT_IP_BIT);
-        esp_wifi_connect();
+        xEventGroupSetBits(wifi_event_group, READY_BIT);
+        ESP_ERROR_CHECK(esp_wifi_connect());
     } else if (event->event_id == SYSTEM_EVENT_STA_CONNECTED) {
         ESP_LOGI(TAG, "Connected to WiFi network %s", event->event_info.connected.ssid);
         xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
@@ -24,15 +27,22 @@ esp_err_t event_handler(void *ctx, system_event_t *event) {
         xEventGroupSetBits(wifi_event_group, GOT_IP_BIT);
         esp_log_level_set("phy_init", ESP_LOG_INFO);
     } else if (event->event_id == SYSTEM_EVENT_STA_DISCONNECTED) {
-        ESP_LOGI(TAG, "Lost WiFi connection, reconnecting...");
+        ESP_LOGI(TAG, "Lost WiFi connection to %s (%d), reconnecting...",
+                 event->event_info.disconnected.ssid, event->event_info.disconnected.reason);
         xEventGroupClearBits(wifi_event_group, CONNECTED_BIT | GOT_IP_BIT);
-        esp_wifi_connect(); // This is a workaround as ESP32 WiFi libs don't currently auto-reassociate.
+        ESP_ERROR_CHECK(esp_wifi_connect());
+        // this is a workaround as ESP32 WiFi libs don't currently auto-reassociate
+    } else if (event->event_id == SYSTEM_EVENT_STA_STOP) {
+        ESP_LOGI(TAG, "WiFi driver shut down");
+        xEventGroupClearBits(wifi_event_group, READY_BIT | CONNECTED_BIT | GOT_IP_BIT);
+    } else {
+        ESP_LOGE(TAG, "Unknown Wifi Event %d", event->event_id);
     }
     return ESP_OK;
 }
 
 void connect_wifi() {
-    nvs_flash_init();
+    ESP_ERROR_CHECK(nvs_flash_init());
     tcpip_adapter_init();
 
     ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
@@ -55,11 +65,38 @@ void connect_wifi() {
 }
 
 esp_err_t await_wifi(TickType_t xTicksToWait) {
-    EventBits_t bits = xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT | GOT_IP_BIT, false, true, portMAX_DELAY);
-    if ((bits & CONNECTED_BIT) > 0 && (bits & GOT_IP_BIT) > 0) {
-        ESP_LOGI(TAG, "WiFi Ready!");
+    EventBits_t bits = xEventGroupWaitBits(wifi_event_group, READY_BIT | CONNECTED_BIT | GOT_IP_BIT,
+                                           false, true, xTicksToWait);
+    if ((bits & READY_BIT) != 0 && (bits & CONNECTED_BIT) != 0 && (bits & GOT_IP_BIT) != 0) {
+        wifi_ap_record_t wifiInfo;
+        ESP_ERROR_CHECK(esp_wifi_sta_get_ap_info(&wifiInfo))
+        ESP_LOGI(TAG, "WiFi connection to %s ready!", wifiInfo.ssid);
         return ESP_OK;
     } else {
         return ESP_ERR_TIMEOUT;
     }
 }
+
+
+esp_err_t ensure_wifi(TickType_t xTicksToWait) {
+    EventBits_t bits;
+    bits = xEventGroupGetBits(wifi_event_group);
+    if ((bits & READY_BIT) != 0 && (bits & CONNECTED_BIT) == 0) {
+        esp_err_t ret = esp_wifi_connect();
+        if (ret != ESP_OK) {
+            return ret;
+        }
+    }
+    bits = xEventGroupWaitBits(wifi_event_group, READY_BIT | CONNECTED_BIT | GOT_IP_BIT, false, true, xTicksToWait);
+    if ((bits & READY_BIT) != 0 && (bits & CONNECTED_BIT) != 0 && (bits & GOT_IP_BIT) != 0) {
+        wifi_ap_record_t wifiInfo;
+        return esp_wifi_sta_get_ap_info(&wifiInfo);
+    } else {
+        if ((bits & READY_BIT) != 0) {
+            return ESP_ERR_TIMEOUT;
+        } else {
+            return ESP_ERR_WIFI_NOT_STARTED;
+        }
+    }
+}
+
--- main/wifi.h
+++ main/wifi.h
@@ -8,4 +8,6 @@ void connect_wifi();
 
 esp_err_t await_wifi(TickType_t xTicksToWait);
 
+esp_err_t ensure_wifi(TickType_t xTicksToWait);
+
 #endif //ESP32_TFA_TEMP_HUM_WIFI_H
